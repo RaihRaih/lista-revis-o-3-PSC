7ba6166f4b395fa1540a600bf58315c9cef1d655
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-04-12 13:35:47 -0300

    Create Questao08.java

---
+++ Questao08.java
@@ -0,0 +1,12 @@
+/*Escreva um código Java que imprima os números de 1 a 10. */
+
+import java.util.Random;
+
+public class Questao08{
+    public static void main(String[] args) {
+
+         for (int i = 1; i <=  10; i++) {
+            System.out.println(i);
+        }
+    }
+}
